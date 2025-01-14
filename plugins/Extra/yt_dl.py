a1442ed92b124b9c2e6d4d5757b868e64784591d
hkteamnoob <hkteamnoob@gmail.com>
2025-01-14 14:39:08 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -1,10 +1,4 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 from __future__ import unicode_literals
-
 import os, requests, asyncio, math, time, wget
 from pyrogram import filters, Client
 from pyrogram.types import Message
